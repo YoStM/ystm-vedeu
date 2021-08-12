39bcbf1f7f3f85e84ba3b5d24dc6e0e03012aa88
YoStM <yo.stm@outlook.com>
2021-08-12 15:29:59 +0200

    Modify footer layout

--- fr/inc/basic_hero_banner.php
+++ fr/inc/basic_hero_banner.php
@@ -1,5 +1,5 @@
 <?php require_once "navbar_fr.php"; ?>
-<section id="<?php echo $hero_id ?>" class="my-5 text-center">
+<section id="<?php echo $hero_id ?>" class="mt-5 p-3 text-center">
     <h1 class="h3"><?php echo $hero_title ?></h1>
     <h2 class="mt-3 h1"><?php echo $hero_subtitle ?></h2>
     <p><?php echo $hero_text ?></p>
