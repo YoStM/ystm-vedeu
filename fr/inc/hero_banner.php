b9fab9adedef604012584be5c6ec44af2e2c85a3
YoStM <yo.stm@outlook.com>
2021-09-20 11:03:09 +0200

    Add hero imm tag and tweak responsive styles

--- fr/inc/hero_banner.php
+++ fr/inc/hero_banner.php
@@ -1,7 +1,8 @@
 <?php require "navbar_fr.php"; ?>
 <section id="hero_banner" class="hero-banner d-flex flex-column justify-content-center align-items-center">
-    <h1 class="h3">Yohan SAINT-MARTIN</h1>
-    <h2 class="mt-3 h1">Développeur web</h2>
+    <img src="../../assets/img/polygons-yo.png" alt="Yohan souriant, la mèche dans le vent">
+    <h1 class="h3 mt-1">Yohan SAINT-MARTIN</h1>
+    <h2 class="h1">Développeur web</h2>
     <p>avec une sensibilité "back-end"</p>
     <a href="#skills">En savoir plus</a>
     <?php require "bubbles.php" ?>
