20ba3036954a1556a39e47d78e6fe7c9464b4640
YoStM <yo.stm@outlook.com>
2022-06-01 12:21:18 +0200

    Modify bubbles

--- fr/inc/bubbles.php
+++ fr/inc/bubbles.php
@@ -2,16 +2,16 @@
 ?>
 <div id="bubbles">
     <div>
-        <h3 class="fs-3">Rails</h3>
+        <img src="../../assets/img/Node.js-Logo.wine.png" alt="Node.js" height="65px" width="95px">
     </div>
     <div></div>
     <div></div>
     <div>
-        <h3 class="fs-5">Symfony</h3>
+        <img src="../../assets/img/Ruby_on_Rails_logo.png" alt="Symfony" height="25px" width="85px">
     </div>
     <div></div>
     <div>
-        <h3 class="fs-1">PHP</h3>
+        <img src="../../assets/img/php-logo.svg" alt="Symfony" height="55px" width="85px">
     </div>
     <div>
         <h3 class="fs-2">SQL</h3>
@@ -19,6 +19,6 @@
     <div></div>
     <div></div>
     <div>
-        <h3 class="fs-3">Ruby</h3>
+        <img src="../../assets/img/symfony-header-logo.svg" alt="Symfony" height="75px" width="95px">   
     </div>
 </div>
